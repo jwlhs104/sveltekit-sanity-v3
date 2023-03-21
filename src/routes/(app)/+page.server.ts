c901561bc84b4222653617ab87a36ca73c0349da
johnny <johnny@DESKTOP-GOPLA46.localdomain>
2023-03-21 20:21:34 +0800

    add page cache

--- src/routes/(app)/+page.server.ts
+++ src/routes/(app)/+page.server.ts
@@ -4,12 +4,16 @@ import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
 // export const prerender = 'auto';
-export const load: PageServerLoad = async ({ parent, params }) => {
+export const load: PageServerLoad = async ({ parent, params, setHeaders }) => {
 	const posts = await getSanityServerClient(false).fetch(allPostsQuery);
 
 	if (!posts) {
 		throw error(500, 'Posts not found');
 	}
+
+	setHeaders({
+		"cache-control": "max-age=60"
+	})
 
 	return {
 		posts
--- src/routes/(app)/posts/[slug]/+page.server.ts
+++ src/routes/(app)/posts/[slug]/+page.server.ts
@@ -4,7 +4,7 @@ import type { Post } from '$lib/types';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async ({ parent, params }) => {
+export const load: PageServerLoad = async ({ parent, params, setHeaders }) => {
 	const { previewMode } = await parent();
 
 	const { post, morePosts } = await getSanityServerClient(previewMode).fetch<{
@@ -17,6 +17,10 @@ export const load: PageServerLoad = async ({ parent, params }) => {
 	if (!post) {
 		throw error(404, 'Post not found');
 	}
+
+	setHeaders({
+		"cache-control": "max-age=60"
+	})
 
 	return {
 		previewMode,
